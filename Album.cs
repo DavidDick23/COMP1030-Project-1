9afe65a4d37363423aa1e56a171515af516c2a45
Dave Dick <60045054+DavidDick23@users.noreply.github.com>
2025-11-12 20:54:29 -0500

    Created Files For Ease Of Use

---
+++ Album.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Constructor
+{
+    //STEP 2: Include a Class that represents a music album featuring the following object properties:
+    public class Album
+    {
+        public string Name { get; private set; } //STEP 3: Name of the album
+        public string Artist { get; private set; } //STEP 4: Name of the artist
+        public int Year { get; private set; } //STEP 5: Year of release
+        public List<string> Tracks { get; private set; } //STEP 6: List of song tracks
+
+        //STEP 7: Build a Class constructor that enables your program to create a new Album object instance, setting each of the above properties properly.
+        public Album(string name, string artist, int year, List<string> tracks)
+        {
+            this.Name = name;
+            this.Artist = artist;
+            this.Year = year;
+            this.Tracks = tracks;
+        }
+
+        //STEP 8: Create a Class method that does the following:
+        public void ShowAlbumDetails()
+        {
+            //STEP 9: Outputs the name of the album, the artist, and the year of release to the console
+
+            //STEP 10: Outputs a neatly-formatted list of all of the album song tracks to the console.
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Constructor;
+
+#region Script Info
+/*  Project1 - Completed By: (Group Names) - 12/11/2025
+ *  
+ *  Credit: We will explain who did what here
+ * 
+ */
+#endregion
+
+#region Steps Completed without Code
+//STEP 1: Create a new C# Console Application.
+//STEP 13: Be sure to include helpful and appropriate comments throughout your program.
+//STEP 14: In your program code comments, be sure to add citations and references for any and all materials that you consulted to help you produce your solution.
+#endregion
+
+//STEP 15: Either in the program comments code or below in the submission comment area below, please answer the following questions:
+#region Questions and Answers
+/*  Question: Did you use generative Artificial Intelligence (AI) to help you develop your program? Why orwhy not?
+ *  Answer: 
+ * 
+ *  Question: If you did use AI - how did you use it? If you did not, how do you think it might help you with a challenge like this?
+ *  Answer: 
+ */
+#endregion
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //STEP 11: Create at least three new object instances of the class Album.
+
+            //STEP 12: Use the above method to output the Album object properties to the console for each Album object.
+        }
+    }
+}
