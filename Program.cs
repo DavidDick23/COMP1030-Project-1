8ced5e94e04a0c0604160d57029225b7ac4d1740
Shy-Anna Lee <shyjah89@gmail.com>
2025-11-29 18:07:38 -0500

    fixes to syntax errors, updated to sepereat album info for album 2 and 3

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Constructor;
 /*  Project1 - Completed By: (Group Names) - 12/11/2025
  *  
  *  Credit: We will explain who did what here
+ Shy-Anna Lee step 11, step 12
  * 
  */
 #endregion
@@ -31,30 +32,38 @@ namespace Project1
     {
         static void Main()
         {
-            //STEP 11: Create at least three new object instances of the class Album. SHY
-            Album album1 = new Album(
+            //STEP 11: Create at least three new object instances of the class Album. SHY-ANNA
+            //Album 1 resources used: https://www.thetoptens.com/music/albums/ for top 10 albums of all time, and Wikipedia for Track list and year of release: https://en.wikipedia.org/wiki/The_Dark_Side_of_the_Moon#Track_listing
+            Album album1 = new Album( 
                 "Dark Side of the Moon",
-                "Pink Floyd"
+                "Pink Floyd",
                 1973,
-                new List,string> {" Speak to Me", "Breathe (In the Air)", "On the Run", "Time, The great Gig in the Sky", "Money, Us and Them", "Any Colour You Like", "Brain Damage", "Eclipse"}
+                new List <string> {" Speak to Me", "Breathe (In the Air)", "On the Run", "Time, The great Gig in the Sky", "Money, Us and Them", "Any Colour You Like", "Brain Damage", "Eclipse"}
                 );
-            
+
+            //Album 2 resources used: https://www.thetoptens.com/music/albums/ for top 10 albums of all time, and Wikipedia for Track list and year of release: https://en.wikipedia.org/wiki/American_Idiot
             Album album2 = new Album(
-                "Dark Side of the Moon",
-                "Pink Floyd"
-                1973,
-                new List,string> {" "}
+                "American Idiot",
+                "Green Day",
+                2004,
+                new List <string> {"American Idiot", "Jesus of Suburbia", "Holiday", "Boulevard of Broken Dreams", "Are We the Waiting", "St. Jimmy", "Give Me Novacaine", "She's a Rebel", "Extraordinary Girl", "Letterbomb", "Wake Me up When September Ends", "Homecoming", "Whatshername"}
                 );
-            
+
+            //Album 3 resources used: https://www.thetoptens.com/music/albums/ for top 10 albums of all time, and Wikipedia for Track list and year of release: https://en.wikipedia.org/wiki/Nevermind
             Album album3 = new Album(
-                "Dark Side of the Moon",
-                "Pink Floyd"
-                1973,
-                new List,string> {" "}
+                "Nevermind",
+                "Nirvana",
+                1991,
+                new List <string> {"Smells Like Teen Spirit", "In Blossom", "Come as You Are", "Breed". "Lithium", "Polly", "Territorial Pissings", "Drain You", "Lounge Act", "Stay Away", "On a Plane", "Something in the Way", "Endless, Nameless"}
                 );
 
             //STEP 12: Use the above method to output the Album object properties to the console for each Album object.
+            album1.DisplayAlbumInfo();
+            album2.DisplayAlbumInfo();
+            album3.DisplayAlbumInfo();
+
         }
     }
 }
+
 
